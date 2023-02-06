9ef5d5c3127e96609817a2ca47f7dc284939b042
Hidde Boekema <h.j.boekema@tudelft.nlX>
2023-02-06 11:15:14 +0100

    Merge

--- map_annotation/utils.py
+++ map_annotation/utils.py
@@ -1,4 +1,14 @@
 
+import numpy as np
+from scipy.interpolate import interp1d, UnivariateSpline
+
+
+def parametric_spline(path_xy, kind):
+    path_t = np.linspace(0, 1, path_xy.size//2)
+    spline = interp1d(path_t, path_xy.T, kind=kind)
+    return spline, path_t
+
+
 def strictly_increasing(L):
     return all(x<y for x, y in zip(L, L[1:]))
 
