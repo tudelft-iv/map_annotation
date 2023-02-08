de348cbe6ce78ff3c94052f33ce2b5e8dc890423
Hidde Boekema <h.j.boekema@tudelft.nlX>
2023-02-08 11:46:36 +0100

    Refactor

--- map_annotation/utils.py
+++ map_annotation/utils.py
@@ -1,27 +1,28 @@
-
 import numpy as np
 from scipy.interpolate import interp1d, UnivariateSpline
 
 
 def parametric_spline(path_xy, kind):
-    path_t = np.linspace(0, 1, path_xy.size//2)
+    path_t = np.linspace(0, 1, path_xy.size // 2)
     spline = interp1d(path_t, path_xy.T, kind=kind)
     return spline, path_t
 
 
 def strictly_increasing(L):
-    return all(x<y for x, y in zip(L, L[1:]))
+    return all(x < y for x, y in zip(L, L[1:]))
+
 
 def strictly_decreasing(L):
-    return all(x>y for x, y in zip(L, L[1:]))
+    return all(x > y for x, y in zip(L, L[1:]))
+
 
 def non_increasing(L):
-    return all(x>=y for x, y in zip(L, L[1:]))
+    return all(x >= y for x, y in zip(L, L[1:]))
+
 
 def non_decreasing(L):
-    return all(x<=y for x, y in zip(L, L[1:]))
+    return all(x <= y for x, y in zip(L, L[1:]))
+
 
 def monotonic(L):
     return non_increasing(L) or non_decreasing(L)
-
-
